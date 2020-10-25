15b4d3bada1b123a1a4486984114fe7f6bf3ce78
@brusnevd <brusnevd@gmail.com>
2020-10-25 12:47:06 +0300

    feat: add func

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let currentValue;
+    bracketsConfig.forEach(() => {
+        let count = bracketsConfig.length
+        while (count > 0) {
+            currentValue = bracketsConfig[count - 1].join('')
+            if (str.includes(currentValue)) {
+                str = str.replace(currentValue, '')
+            } else {
+                count--
+            }
+        }
+    })
+    return str.length === 0 ? true : false
 }
